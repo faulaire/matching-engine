b66b77ecafaf293d12ec4a660116095c63f0d6e8
Fabien Aulaire <Vladec@users.noreply.github.com>
2015-03-19 13:27:34 +0100

    Remove useless break statement

--- matching-engine/src/Engine_OrderBook.cpp
+++ matching-engine/src/Engine_OrderBook.cpp
@@ -16,19 +16,14 @@ namespace exchange
             {
                 case TradingPhase::OPENING_AUCTION:
                     return "OPENING_AUCTION";
-                    break;
                 case TradingPhase::CONTINUOUS_TRADING:
                     return "CONTINUOUS_TRADING";
-                    break;
                 case TradingPhase::CLOSING_AUCTION:
                     return "CLOSING_AUCTION";
-                    break;
                 case TradingPhase::CLOSE:
                     return "CLOSE";
-                    break;
                 case TradingPhase::INTRADAY_AUCTION:
                     return "INTRADAY_AUCTION";
-                    break;
                 default:
                     return "UNKNOW_TRADING_PHASE";
             }
