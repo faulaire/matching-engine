55176f46ab9700a8942256063be365804718be94
fabien <fabien.aulaire@free.fr>
2015-04-30 22:53:21 +0200

    - Remove useless Tools - Instrument loading is now exception safe - Rework phase swithing mechanism - Cancel all non persistent orders after closing auction - Test unit rework firt pass - Various bug fixes

---
+++ common/include/ScopedExit.h
@@ -0,0 +1,32 @@
+#include <functional>
+
+#include <boost/utility.hpp>
+
+namespace exchange
+{
+    namespace common
+    {
+        template <typename TCallback>
+        struct scope_exit : boost::noncopyable
+        {
+            scope_exit(const TCallback & f) : f_(f)
+            {}
+            
+            scope_exit(scope_exit && rhs):
+                f_(std::move(rhs.f_))
+            {}
+
+            ~scope_exit(void) { f_(); }
+        private:
+            TCallback f_;
+        };
+
+
+        template <typename TCallback>
+        auto make_scope_exit(const TCallback & callback)
+        {
+            return scope_exit<TCallback>( callback );
+        }
+
+    }
+}
