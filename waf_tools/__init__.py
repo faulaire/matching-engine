f7b12c46c6167537e77ab94d3bf00c857a6e6d27
Fabien Aulaire <faulaire@pc-ubuntu.home>
2016-04-19 23:47:49 +0200

    - Remove vera and rats analysis

--- waf_tools/__init__.py
+++ waf_tools/__init__.py
@@ -84,21 +84,4 @@ def run_tests(Context):
     Context.exec_command( "gcovr -x -r . > %s/gcovr-report.xml" % (bin_dir) )
     Context.exec_command( "gcovr -r . --html --html-details -o %s/coverage-report.html" % (bin_dir) )
 
-    # Run cppcheck, a general purpose static code checker
-    sources_path = "-I%s/include %s/src"%(Context.path.relpath(), Context.path.relpath())
-    rats_cmd = "cppcheck --enable=all --inconclusive --xml-version=2 --suppress=missingIncludeSystem %s  2> %s/cppcheck-report.xml"%(sources_path, bin_dir)
-    Context.exec_command(rats_cmd)
-    
-    # Run rats: static code checker focusing on (potential) security problems
-    Context.exec_command("rats -w 3 --xml %s > %s/rats-report.xml"%(Context.path.relpath(), bin_dir))
-    
-    # Run vera++: static code checker focusing on code style issues
-    
-    # All files but remove unit tests
-    files_to_analyze = " find %s -regex \".*\.cpp\|.*\.h\"|grep -v 'test_' "%(Context.path.relpath())
-    vera_command     = " vera++ - -showrules -nodup"
-    vera_parser_cmd  = " vera++Report2checkstyleReport.perl > %s/vera++-report.xml"%(bin_dir)
-
-    Context.exec_command("bash -c '%s|%s|&%s'"%(files_to_analyze,vera_command,vera_parser_cmd))
-
     os.chdir( saved_dir )
